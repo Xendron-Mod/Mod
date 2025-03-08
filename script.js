3917f83fb786e765c20d6f6d84060e41ad9f7cc5
XenDron <achus1379@gmail.com>
2025-03-08 09:16:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Website Loaded!");
+document.getElementById("darkModeToggle").addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
 });
