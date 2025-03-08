5a94160ee3c55aadb3a26a54e10d104476a01772
XenDron <achus1379@gmail.com>
2025-03-08 09:26:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-document.getElementById("darkModeToggle").addEventListener("click", function () {
+// Dark Mode Toggle
+const toggleButton = document.getElementById("darkModeToggle");
+toggleButton.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 });
